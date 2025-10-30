554054e912d4b97e410e7276d4eb753da9bab405
Maxime Normandin <maxnorm@hotmail.com>
2025-10-29 21:20:46 -0400

    update url

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docusaurus-test-8dzcobyq3-maxnorms-projects.vercel.app/',
+  url: 'https://docusaurus-test-seven-sage.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
