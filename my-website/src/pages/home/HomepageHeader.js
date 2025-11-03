bedef088f191d196675ef71439af5a6ad944144a
Maxime Normandin <maxnorm@hotmail.com>
2025-11-03 12:39:15 -0500

    fix wave color on both theme

--- my-website/src/pages/home/HomepageHeader.js
+++ my-website/src/pages/home/HomepageHeader.js
@@ -66,7 +66,9 @@ export default function HomepageHeader() {
         </div>
       </div>
       <div className={styles.heroWave}>
-        <img src="/icons/wave.svg" alt="" aria-hidden="true" />
+        <svg viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+          <path d="M0 0L60 10C120 20 240 40 360 46.7C480 53 600 47 720 43.3C840 40 960 40 1080 46.7C1200 53 1320 67 1380 73.3L1440 80V120H1380C1320 120 1200 120 1080 120C960 120 840 120 720 120C600 120 480 120 360 120C240 120 120 120 60 120H0V0Z" fill="currentColor"/>
+        </svg>
       </div>
     </header>
   );
